b0273d3da7ee13625d854c3adb38a1e579b39858
Tuomas Kuosmanen <tigert@redhat.com>
2016-03-16 14:27:03 +0200

    yum is giving way to dnf, handle both cases

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
-
+#!/usr/bin/env sh
 git submodule init && git submodule update
 
-sudo yum install -y ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch ImageMagick
+DEPENDENCIES="ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch ImageMagick"
+
+if hash dnf 2>/dev/null; then
+  sudo dnf install $DEPENDENCIES
+else
+  sudo yum install $DEPENDENCIES 
+fi
 
 bundle install
+
